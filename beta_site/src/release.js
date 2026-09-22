d8a6a2c12822a665f97b0bf80cf22115f8c36e4c
Daria Agafonova <33396527+viranovskaya@users.noreply.github.com>
2026-09-22 16:53:55 +0200

    Serve the published v0.3.0b2 researcher beta kit

--- beta_site/src/release.js
+++ beta_site/src/release.js
@@ -1,10 +1,10 @@
 export const RELEASE = Object.freeze({
-  tag: "v0.3.0b1",
-  version: "0.3.0b1",
-  wheel: "neurodata_release_security_audit-0.3.0b1-py3-none-any.whl",
-  wheelSha256: "24a69039ffb9152b3f2ae7cf81d3fe4d13ad6e540e1c0e91d209ecb5bd15e149",
-  kitRevision: "r3",
-  archive: "neurodata-researcher-beta-0.3.0b1-r3.zip",
-  archiveSha256: "167dd13cd72e6beeb955729821100dbb275e0a9d084637d51ab042bd51b9b8b7",
-  releaseUrl: "https://github.com/viranovskaya/neurodata-release-security-audit/releases/tag/v0.3.0b1",
+  tag: "v0.3.0b2",
+  version: "0.3.0b2",
+  wheel: "neurodata_release_security_audit-0.3.0b2-py3-none-any.whl",
+  wheelSha256: "00a286b39b37c1f91bf6f460b0bfa7f1755e14c534b4ddada2fbca3d1ef0d714",
+  kitRevision: "r1",
+  archive: "neurodata-researcher-beta-0.3.0b2-r1.zip",
+  archiveSha256: "2af9dfd9db37fb67f14f25312072aafa6abcb66e3e4ef1e5a58d9dee8866fd87",
+  releaseUrl: "https://github.com/viranovskaya/neurodata-release-security-audit/releases/tag/v0.3.0b2",
 });
